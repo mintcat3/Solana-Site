e450a2d948a6afc03c924a5d35a28650afe71045
PhuNN16 <phunick316@gmail.com>
2023-10-14 16:44:00 -0700

    fix solana page quiz to allow string fixing

--- js files/solana_page_quiz.js
+++ js files/solana_page_quiz.js
@@ -3,50 +3,47 @@ function checkbutton(question_number) {
     return document.querySelector(name);
 }  
 
-function format_result(question, result) {
+function format_result(question, result, string) {
 document.getElementById(question).style.borderStyle = 'solid'
 document.getElementById(question).style.borderWidth = '2px 0px 0px 10px'
 document.getElementById(question).style.width = '30%'
 if (result == true) {
-    document.getElementById(question).innerHTML = " correct";
+    document.getElementById(question).innerHTML = string;
     document.getElementById(question).style.borderColor = 'rgb(49, 195, 49)'
 } else {
-    document.getElementById(question).innerHTML = " incorrect";
+    document.getElementById(question).innerHTML = string;
     document.getElementById(question).style.borderColor = 'red'
 }
 }
 
 document.getElementById("question1").addEventListener("click", function(event) {
-    console.log("clicked");
-    // document.getElementById("q1answer").innerHTML = "incorrect";
     let choice = checkbutton("1").value
-    // console.log(choice)
+    let correct_string = 'Correct: '
+    let incorrect_string = 'Incorrect: '
     if (choice == 'firstchoice') {
-        format_result("q1answer", true)
+        format_result("q1answer", true, correct_string)
     } else {
-        format_result("q1answer", false)
+        format_result("q1answer", false, incorrect_string)
     }
 })
 
 
 document.getElementById("question2").addEventListener("click", function(event) {
-    // console.log("clicked");
-    // document.getElementById("q1answer").innerHTML = "incorrect";
     let choice = checkbutton("2").value
-    // console.log(choice)
+    let correct_string = 'Correct: '
+    let incorrect_string = 'Incorrect: '
     if (choice == 'firstchoice') {
-        format_result("q2answer", true)
+        format_result("q2answer", true, correct_string)
     } else {
-        format_result("q2answer", false)
+        format_result("q2answer", false, incorrect_string)
     }
 })
 
 
 document.getElementById("question3").addEventListener("click", function(event) {
-    // console.log("clicked");
-    // document.getElementById("q1answer").innerHTML = "incorrect";
     let choice = checkbutton("3").value
-    // console.log(choice)
+    let correct_string = 'Correct: '
+    let incorrect_string = 'Incorrect: '
     if (choice == 'firstchoice') {
         format_result("q3answer", true)
     } else {
